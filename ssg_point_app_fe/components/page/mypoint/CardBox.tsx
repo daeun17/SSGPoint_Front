6f12948c1cfe176c64eaa202c7e3fc4aa1a3fa0c
daeunk17 <daeunk71@gmail.com>
2023-08-28 14:17:57 +0900

    [fix]merge error fix

--- ssg_point_app_fe/components/page/mypoint/CardBox.tsx
+++ ssg_point_app_fe/components/page/mypoint/CardBox.tsx
@@ -13,7 +13,6 @@ export default function CardBox() {
             <h3 className={style.tit_img}>SHINSEGAE POINT</h3>
             <button className={style.btn_info}></button> {/*todo 유형에 따라 버튼 다른거 띄우기 , 바코드, 안내*/}
         </div>
-        {/*todo 유형에 따라 버튼 다른거 띄우기 , 바코드, 안내*/}
         {pathname === "/mypoint/pntHistory"?<PointHistoryCard/> :null}
     </div>
   )
