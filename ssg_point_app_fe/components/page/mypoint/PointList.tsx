abcd2e391e16838304961dd7f215707c9a88b86a
daeunk17 <daeunk71@gmail.com>
2023-09-01 16:33:33 +0900

    [fix] point query parameter fix

--- ssg_point_app_fe/components/page/mypoint/PointList.tsx
+++ ssg_point_app_fe/components/page/mypoint/PointList.tsx
@@ -1,16 +1,58 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import style from "./MyPoint.module.css"
 import FilterMenu from './SortRadio'
-import PointFilter from './PointFilter'
+import PointFilter, { dateFormat } from './PointFilter'
 import PointWrap from './PointWrap'
 import SortRadio from './SortRadio'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
+
+export interface sortKeysType {
+  pointType: string,
+  rangeStartDate: string,
+  rangeEndDate: string,
+  usedType: string,
+  pointHistoryType: string
+}
 export default function PointList() {
+
+  const queryParam = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const nowdate = new Date(); 
+  const defultdate = new Date();// 현재 날짜 조회 
+  defultdate.setMonth(nowdate.getMonth()-1); // 기본 설정 날짜 
+  
+  const [querySort, setQuerySort] = useState<sortKeysType>({
+    pointType: queryParam.get('pointType'),
+    rangeStartDate: queryParam.get('rangeStartDate'),
+    rangeEndDate: queryParam.get('rangeEndDate'),
+    usedType: queryParam.get('usedType'),
+    pointHistoryType: queryParam.get('pointHistoryType')
+  } as sortKeysType);
+
+  useEffect(()=>{
+    // 이거쓰면 query 다가져 옴 
+    if( queryParam.toString() === `pointType=${querySort.pointType}&rangeStartDate=${querySort.rangeStartDate}&rangeEndDate=${querySort.rangeEndDate}&usedType=${querySort.usedType}&pointHistoryType=${querySort.pointHistoryType}`) {
+      return 
+    }
+    console.log('open')
+    router.push(
+      `${pathname}?pointType=${querySort.pointType}&rangeStartDate=${querySort.rangeStartDate}&rangeEndDate=${querySort.rangeEndDate}&usedType=${querySort.usedType}&pointHistoryType=${querySort.pointHistoryType}`
+    )
+  },[querySort])
 
   return (
     <div className={style.point_list_box}>
-      <SortRadio/>
-      <PointFilter/>
+      <SortRadio 
+        querySort = {querySort}
+        setQuerySort = {setQuerySort}
+      />
+      <PointFilter
+        querySort = {querySort}
+        setQuerySort = {setQuerySort}
+      />
       <PointWrap/>
 
     </div>
