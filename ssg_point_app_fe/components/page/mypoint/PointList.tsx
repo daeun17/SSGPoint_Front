6a8dac17ca2bf215ece31a4dd27635ad3277be99
daeunk17 <daeunk71@gmail.com>
2023-09-01 18:50:45 +0900

    [docs] point query 관련 comment작성

--- ssg_point_app_fe/components/page/mypoint/PointList.tsx
+++ ssg_point_app_fe/components/page/mypoint/PointList.tsx
@@ -17,13 +17,16 @@ export interface sortKeysType {
 }
 export default function PointList() {
 
+
   const queryParam = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
-  const nowdate = new Date(); 
-  const defultdate = new Date();// 현재 날짜 조회 
+
+  const nowdate = new Date(); // 현재 날짜 조회 
+  const defultdate = new Date(); 
   defultdate.setMonth(nowdate.getMonth()-1); // 기본 설정 날짜 
   
+  // query parameter 들 상태 관리 및 기본값 설정 
   const [querySort, setQuerySort] = useState<sortKeysType>({
     pointType: queryParam.get('pointType'),
     rangeStartDate: queryParam.get('rangeStartDate'),
@@ -34,6 +37,7 @@ export default function PointList() {
 
   useEffect(()=>{
     // 이거쓰면 query 다가져 옴 
+    // 해당 부분을 넣으니까 초기에 들어 갈 때 데이터를 두번 로드하는 현상이 해결 됨 
     if( queryParam.toString() === `pointType=${querySort.pointType}&rangeStartDate=${querySort.rangeStartDate}&rangeEndDate=${querySort.rangeEndDate}&usedType=${querySort.usedType}&pointHistoryType=${querySort.pointHistoryType}`) {
       return 
     }
@@ -45,6 +49,7 @@ export default function PointList() {
 
   return (
     <div className={style.point_list_box}>
+      {/* 상태 관리하는 query parameter들을 하위 컴포넌트로 전달하기 */}
       <SortRadio 
         querySort = {querySort}
         setQuerySort = {setQuerySort}
