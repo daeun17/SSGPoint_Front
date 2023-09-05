3a833e3b95c5d2c27db8302b9b0c7feb5354194d
daeunk17 <daeunk71@gmail.com>
2023-09-05 18:43:39 +0900

    [Feat] point password change fetch complet

---
+++ ssg_point_app_fe/components/page/mypoint/savePoint/SavePoint.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import style from './SavePoint.module.css'
+
+export default function SavePoint() {
+  return (
+    <>
+    <div className={style.org_txt_box}>
+        <h3 className={style.box_tit}>
+            포인트 적립을 깜빡하셨나요? <br />
+            <strong className={style.fc_pk}>영수증 정보만 입력해 주세요.</strong>
+        </h3>
+        <p className={style.box_txt}>
+            이마트, 신세계백화점, 이마트 에브리데이에서<br />
+            구매하신 영수증 정보로 포인트 적립이 가능합니다.
+        </p>
+    </div>
+    <div className={style.mypoint_cnt}>
+        <p className={style.sp_line_tit0}> 영수증 정보 등록</p>
+        
+
+    </div>
+    </>
+  )
+}
