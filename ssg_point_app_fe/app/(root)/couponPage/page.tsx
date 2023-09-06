b742b92b6ab27a83aeaa9355bd439a3724f758b0
daeunk17 <daeunk71@gmail.com>
2023-09-06 18:42:24 +0900

    [Feat] coupon component

--- ssg_point_app_fe/app/(root)/couponPage/page.tsx
+++ ssg_point_app_fe/app/(root)/couponPage/page.tsx
@@ -1,10 +1,14 @@
 
 import React from 'react'
 import CouponHeader from '@/components/page/coupon/CouponHeader';
+import CouponListWrap from '@/components/page/coupon/CouponListWrap';
 
 
 export default async function page() {
   return (
-    <CouponHeader/>
+    <>
+      <CouponHeader/>
+      <CouponListWrap/>
+    </>
   )
 }
---
+++ ssg_point_app_fe/types/CouponType.ts
@@ -0,0 +1,14 @@
+export interface CouponType {
+    "partnerName": string,
+    "imageUrl": string,
+    "imageUrlCircle": string,
+    "couponMerchandise": string,
+    "couponId": number,
+    "couponName": string,
+    "couponStart": Date,
+    "couponEnd": Date,
+    "couponImg": string,
+    "couponPrecaution": string,
+    "useStatus": boolean,
+    "couponNumber": string
+}
